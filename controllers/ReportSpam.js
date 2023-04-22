bd07cfb19590b6d2dc7bf58ffa1703f459003ba2
nipunkhattri321 <nipunkhattri321@gmail.com>
2023-04-23 02:46:47 +0530

    Api

--- controllers/ReportSpam.js
+++ controllers/ReportSpam.js
@@ -2,19 +2,21 @@ import CallSchema from "../models/Callspan.js"
 
 export const ReportSpam = async (req,res) =>{
     const numbers = req.body.Phone_no
-    console.log(numbers);
+    // console.log(numbers);
     const NoG = 0;
     const NoR = 0 ;
     let score = 0;
     const phones = await CallSchema.find({ Phone_no: { $in: numbers } })
-    console.log(phones);
+    // console.log(phones);
     const status = numbers.map(number => {
       const phone = phones.find(p => p.Phone_no === parseInt(number))
       console.log(phone)
-      const phones1 = CallSchema.find({ phone })
+      const phones1 =  CallSchema.find({ phone })
+      // console.log(phones1.NumberofReports)
       if (phone){
-        score = (phones1.NumberofReports/(phones1.NumberofGenuie+phones1.NumberofReports))*100;
-        if (phones1.NumberofReports > 3) {
+        console.log(phone.NumberofReports);
+        score = (phone.NumberofReports/(phone.NumberofGenuie+phone.NumberofReports))*100;
+        if (phone.NumberofReports > 3) {
           return { number, status: 'Report',score }
         } else {
           return { number, status: 'Not Report',score }
