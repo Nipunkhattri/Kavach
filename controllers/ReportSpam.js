08bace0344a4e54ca6d54612681e77fb0c148156
nipunkhattri321 <nipunkhattri321@gmail.com>
2023-04-29 19:55:57 +0530

    added clear data

--- controllers/ReportSpam.js
+++ controllers/ReportSpam.js
@@ -1,4 +1,7 @@
+// import { Phone } from "@material-ui/icons";
 import CallSchema from "../models/Callspan.js"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const ReportSpam = async (req,res) =>{
     const numbers = req.body.Phone_no
@@ -8,11 +11,13 @@ export const ReportSpam = async (req,res) =>{
     let score = 0;
     const phones = await CallSchema.find({ Phone_no: { $in: numbers } })
     // console.log(phones);
-    const status = numbers.map(number => {
+    const status =  numbers.map(number => {
       const phone = phones.find(p => p.Phone_no === parseInt(number))
       console.log(phone)
       const phones1 =  CallSchema.find({ phone })
       // console.log(phones1.NumberofReports)
+      // const hashNumber =  bcrypt.hash(phones1.number, 12);
+      // console.log(hashNumber)
       if (phone){
         console.log(phone.NumberofReports);
         score = Math.floor((phone.NumberofReports/(phone.NumberofGenuie+phone.NumberofReports))*100);
@@ -23,6 +28,7 @@ export const ReportSpam = async (req,res) =>{
         }
       }
       else{
+        // const hashNumber =  bcrypt.hash(number, 12);
         const newPhone = new CallSchema({ Phone_no:number, NumberofReports:NoR,NumberofGenuie:NoG})
         score = Math.floor((newPhone.NumberofReports/(newPhone.NumberofGenuie+newPhone.NumberofReports))*100);
         newPhone.save()
@@ -31,4 +37,13 @@ export const ReportSpam = async (req,res) =>{
     //   i++;
     })
     res.json( status )  
-  }
+}
+
+export const ClearData = async (req,res) =>{
+  CallSchema.deleteMany().then(function(){
+    console.log("Data deleted");
+    res.status(200).json({message:"data deleted"}); 
+  }).catch(function(error){
+    console.log(error); 
+});
+}
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 import {CallCheck} from "../controllers/Spancall.js";
 import {GenuieCheck} from "../controllers/Spancall.js";
 import {ReportSpam} from "../controllers/ReportSpam.js";
+import {ClearData} from "../controllers/ReportSpam.js";
 import express from "express";
 const router = express.Router();
 
@@ -8,5 +9,6 @@ const router = express.Router();
 router.post("/call",CallCheck)
 router.post("/Gen",GenuieCheck)
 router.post("/Report",ReportSpam)
+router.post("/Clear",ClearData)
 
 export default router;
